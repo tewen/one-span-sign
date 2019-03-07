14e4b6c8f011f84209d9cddc9ab06206b67da3fa
Trevor Ewen <tewen@Trevors-MacBook-Pro-3.local>
2019-03-07 08:50:00 -0500

    Integrated the deep-cuts module for utility support.

--- test/unit/util/object.spec.js
+++ test/unit/util/object.spec.js
@@ -1,31 +1,7 @@
 const { expect } = require('chai');
-const { safeJsonParse, alwaysArray, splitOrArray } = require('../../../lib/util/object');
+const { alwaysArray, splitOrArray } = require('../../../lib/util/object');
 
 describe('util/object', function () {
-  describe('safeJsonParse()', function () {
-    it('should return an empty object if passed undefined', function () {
-      expect(safeJsonParse(undefined)).to.eql({});
-    });
-
-    it('should return an empty object if passed null', function () {
-      expect(safeJsonParse(null)).to.eql({});
-    });
-
-    it('should return an empty object if passed an invalid JSON string', function () {
-      expect(safeJsonParse('My name is Steve')).to.eql({});
-    });
-
-    it('should be able to parse stringified JSON', function () {
-      expect(safeJsonParse(JSON.stringify({
-        red: true,
-        blue: 10
-      }))).to.eql({
-        red: true,
-        blue: 10
-      });
-    });
-  });
-
   describe('alwaysArray()', function () {
     it('should return an item in an array if passed a non-array', function () {
       expect(alwaysArray(77)).to.eql([77]);
